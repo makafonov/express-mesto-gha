6fe6e3e256af5285029d69539a54400bbb757de5
Denis S <makafonov@gmail.com>
2022-07-26 20:56:23 +0500

    Тулинг

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+};
