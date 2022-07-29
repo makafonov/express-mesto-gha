c1485dea2e68f9b7ec017720c5afb920284e477b
Denis S <makafonov@gmail.com>
2022-07-29 09:33:48 +0500

    Перемещен app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { routes } = require('./routes');
+const { routes } = require('./src/routes');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
