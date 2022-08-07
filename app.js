4d15a6e0a208cee6ed5a6b410e6f1684d8e0a54f
Denis S <makafonov@gmail.com>
2022-08-07 10:48:08 +0500

    Удалено требование к минимальной длине пароля 🤯

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().email().required(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required(),
     }),
   }),
   userController.login,
@@ -28,7 +28,7 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().email().required(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri({
